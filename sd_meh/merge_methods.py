594b0df3cb0c058b8d091822623ae5d99fedda41
ljleb <set>
2023-05-30 02:35:46 -0400

    move methods to own file

---
+++ sd_meh/merge_methods.py
@@ -0,0 +1,52 @@
+import torch
+
+
+__all__ = [
+    "weighted_sum",
+    "weighted_subtraction",
+    "tensor_sum",
+    "add_difference",
+    "sum_twice",
+    "triple_sum",
+]
+
+
+EPSILON = 1e-10  # Define a small constant EPSILON to prevent division by zero
+
+
+def weighted_sum(a: torch.Tensor, b: torch.Tensor, alpha: float, **kwargs) -> torch.Tensor:
+    return (1 - alpha) * a + alpha * b
+
+
+def weighted_subtraction(a: torch.Tensor, b: torch.Tensor, alpha: float, beta: float, **kwargs) -> torch.Tensor:
+    # Adjust beta if both alpha and beta are 1.0 to avoid division by zero
+    if alpha == 1.0 and beta == 1.0:
+        beta -= EPSILON
+
+    return (a - alpha * beta * b) / (1 - alpha * beta)
+
+
+def tensor_sum(a: torch.Tensor, b: torch.Tensor, alpha: float, beta: float, **kwargs) -> torch.Tensor:
+    if alpha + beta <= 1:
+        tt = a.clone()
+        talphas = int(a.shape[0] * beta)
+        talphae = int(a.shape[0] * (alpha + beta))
+        tt[talphas:talphae] = b[talphas:talphae].clone()
+    else:
+        talphas = int(a.shape[0] * (alpha + beta - 1))
+        talphae = int(a.shape[0] * beta)
+        tt = b.clone()
+        tt[talphas:talphae] = a[talphas:talphae].clone()
+    return tt
+
+
+def add_difference(a: torch.Tensor, b: torch.Tensor, c: torch.Tensor, alpha: float, **kwargs) -> torch.Tensor:
+    return a + alpha * (b - c)
+
+
+def sum_twice(a: torch.Tensor, b: torch.Tensor, c: torch.Tensor, alpha: float, beta: float, **kwargs) -> torch.Tensor:
+    return (1 - beta) * ((1 - alpha) * a + alpha * b) + beta * c
+
+
+def triple_sum(a: torch.Tensor, b: torch.Tensor, c: torch.Tensor, alpha: float, beta: float, **kwargs) -> torch.Tensor:
+    return (1 - alpha - beta) * a + alpha * b + beta * c
