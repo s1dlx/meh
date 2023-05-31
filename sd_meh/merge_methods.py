c699dca9ecfcac0d91de7abe1017f3ffb7611319
ljleb <set>
2023-05-31 04:43:43 -0400

    dist crossover

--- sd_meh/merge_methods.py
+++ sd_meh/merge_methods.py
@@ -1,3 +1,4 @@
+import torch
 from torch import Tensor
 
 __all__ = [
@@ -7,6 +8,7 @@ __all__ = [
     "add_difference",
     "sum_twice",
     "triple_sum",
+    "distribution_crossover"
 ]
 
 
@@ -55,3 +57,30 @@ def triple_sum(
     a: Tensor, b: Tensor, c: Tensor, alpha: float, beta: float, **kwargs
 ) -> Tensor:
     return (1 - alpha - beta) * a + alpha * b + beta * c
+
+
+def distribution_crossover(
+    a: Tensor, b: Tensor, c: Tensor, alpha: float, beta: float, **kwargs
+):
+    if a.shape == ():
+        return alpha * a + (1 - alpha) * b
+
+    c_indices = torch.argsort(torch.flatten(c))
+    a_dist = torch.gather(torch.flatten(a), 0, c_indices)
+    b_dist = torch.gather(torch.flatten(b), 0, c_indices)
+
+    a_dft = torch.fft.rfft(a_dist.float())
+    b_dft = torch.fft.rfft(b_dist.float())
+
+    dft_filter = torch.arange(0, torch.numel(a_dft), device=a_dft.device).float()
+    dft_filter /= torch.numel(a_dft)
+    if beta > EPSILON:
+        dft_filter = (dft_filter - alpha) / beta + 1/2
+        dft_filter = torch.clamp(dft_filter, 0., 1.)
+    else:
+        dft_filter = (dft_filter >= alpha).float()
+
+    x_dft = (1 - dft_filter) * a_dft + dft_filter * b_dft
+    x_dist = torch.fft.irfft(x_dft, a_dist.shape[0])
+    x_values = torch.gather(x_dist, 0, torch.argsort(c_indices))
+    return x_values.reshape_as(a)
