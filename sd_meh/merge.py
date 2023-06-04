9ed297b21b25528c402f70538636c6b1284e8a40
s1dlx <s1dlx@proton.me>
2023-06-04 12:42:22 +0100

    prune vae only

--- sd_meh/merge.py
+++ sd_meh/merge.py
@@ -80,7 +80,7 @@ def prune_sd_model(model: Dict) -> Dict:
         if (
             not k.startswith("model.diffusion_model.")
             # and not k.startswith("first_stage_model.")
-            # and not k.startswith("cond_stage_model.")
+            and not k.startswith("cond_stage_model.")
         ):
             del model[k]
     return model
