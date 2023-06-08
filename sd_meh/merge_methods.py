1e4bec7aa686151146e0426e0b7a9befac543f6a
ljleb <set>
2023-06-08 01:16:20 -0400

    black

--- sd_meh/merge_methods.py
+++ sd_meh/merge_methods.py
@@ -82,7 +82,7 @@ def euclidean_add_difference(
 def multiply_difference(
     a: Tensor, b: Tensor, c: Tensor, alpha: float, beta: float, **kwargs
 ) -> Tensor:
-    diff_a = torch.abs(a.float() - c) ** (1-alpha)
+    diff_a = torch.abs(a.float() - c) ** (1 - alpha)
     diff_b = torch.abs(b.float() - c) ** alpha
     difference = diff_a * diff_b
     difference = torch.copysign(difference, weighted_sum(a, b, beta) - c)
