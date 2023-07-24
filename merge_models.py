921234704382bd5fcba671266a44567d993b76fd
s1dlx <s1dlx@proton.me>
2023-07-24 14:42:12 +0100

    add utils

--- merge_models.py
+++ merge_models.py
@@ -4,7 +4,7 @@ import click
 
 from sd_meh.merge import merge_models, save_model
 from sd_meh.presets import BLOCK_WEIGHTS_PRESETS
-from utils import MERGE_METHODS, weights_and_bases
+from sd_meh.utils import MERGE_METHODS, weights_and_bases
 
 
 @click.command()
