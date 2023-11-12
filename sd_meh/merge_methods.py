cbb6a06e5fbc2b1585e60122e2148de019a73743
ljleb <lebel.louisjacob@gmail.com>
2023-11-12 02:45:48 -0500

    optimize: 45m -> 7m

--- sd_meh/merge_methods.py
+++ sd_meh/merge_methods.py
@@ -217,9 +217,9 @@ def rotate(a: Tensor, b: Tensor, alpha: float, **kwargs):
     if len(a.shape) == 0 or (a == b).all():
         return a
 
-    a_2d = a.reshape(-1, a.shape[-1]).double()
-    b_2d = b.reshape(-1, b.shape[-1]).double()
-    u, _, v_t = torch.linalg.svd(torch.matmul(a_2d.T, b_2d))
+    a_2d = a.reshape(-1, a.shape[-1]).float()
+    b_2d = b.reshape(-1, b.shape[-1]).float()
+    u, _, v_t = torch.linalg.svd(torch.matmul(a_2d.T, b_2d), driver="gesvd" if a.is_cuda else None)
     transform = torch.matmul(u, v_t)
 
     if alpha == round(alpha):
@@ -227,11 +227,12 @@ def rotate(a: Tensor, b: Tensor, alpha: float, **kwargs):
             torch.linalg.matrix_power(transform, round(alpha), out=transform)
     else:
         u[:, -1] *= torch.det(transform)
-        torch.matmul(u, v_t, out=transform)
+        transform.copy_(torch.matmul(u, v_t))
         transform.copy_(fractional_matrix_power(transform, alpha))
 
-    torch.matmul(a_2d, transform, out=a_2d)
-    return a_2d.reshape_as(a).to(dtype=a.dtype)
+    torch.matmul(a_2d.float(), transform, out=a_2d)
+    res = a_2d.reshape_as(a).to(dtype=a.dtype)
+    return res
 
 
 def fractional_matrix_power(matrix: Tensor, power: float):
@@ -241,4 +242,4 @@ def fractional_matrix_power(matrix: Tensor, power: float):
         eigenvectors @ torch.diag(eigenvalues) @ eigenvectors.H
     )
     print(f"complex error: {torch.linalg.norm(result.imag)}")
-    return result.real.to(dtype=matrix.dtype)
+    return result.real
